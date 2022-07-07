2871e0e2630a387f4749e6f24efd90b896db91d2
Jerry Thomas <me@jerrythomas.name>
2022-07-07 18:01:02 +0530

    Upgraded all packages. Marked playwright tests as e2e

--- sites/playground/playwright.config.js
+++ sites/playground/playwright.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
 	webServer: {
-		command: 'npm run build && npm run preview',
+		command: 'pnpm build && pnpm preview',
 		port: 3000
 	}
 }
