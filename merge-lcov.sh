3eb7d9bf6241d45b8fb5e26e4658a37e13b84110
Jerry Thomas <me@jerrythomas.name>
2022-07-14 14:30:44 +0530

    Used shared vitest config and script to merge lcov.info

---
+++ merge-lcov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+files=`find ./packages  -name lcov.info`
+args=""
+for f in $files; do
+  prefix=`echo $f | sed -e s/coverage.lcov.info// | sed -e s#^\./##`
+	echo "fixing paths in $f"
+	sed -i.bak "s#^SF\:src#SF\:${prefix}src#g" $f
+	args="$args -a $f"
+done
+echo $args | xargs lcov -o lcov.info
