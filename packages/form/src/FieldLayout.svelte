f8bffefcd7d9502ce1bd866fc615770616505335
Jerry Thomas <me@jerrythomas.name>
2022-09-28 17:25:37 +0530

    Upgraded depenencies

--- packages/form/src/FieldLayout.svelte
+++ packages/form/src/FieldLayout.svelte
@@ -1,9 +1,7 @@
 <script>
-	let className = 'flex flex-col'
-	export { className as class }
 	export let layout = 'vertical'
 </script>
 
-<field-layout class="{layout} {className}">
+<field-layout class={layout}>
 	<slot />
 </field-layout>
