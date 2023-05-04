c3fbfe7da67208bbf9987c1da397f5595df0376f
Jerry Thomas <me@jerrythomas.name>
2023-05-04 17:20:25 +0530

    Updated the sidebar styles for different sizes. Made nav bar thinner

--- sites/learn/src/routes/(learn)/Sidebar.svelte
+++ sites/learn/src/routes/(learn)/Sidebar.svelte
@@ -20,11 +20,11 @@
 
 <aside
 	bind:this={sidebar}
-	class="flex flex-col gap-4 bg-skin-base h-full flex-shrink-0 w-80 bg-skin-base absolute top-0 left-0 -translate-x-full transform transition duration-200 ease-in-out z-5 border-r border-skin-subtle overflow-auto"
+	class="flex flex-col gap-4 bg-skin-base h-full flex-shrink-0 w-full md:w-1/2 lg:w-80 bg-skin-base absolute top-0 left-0 -translate-x-full transform transition duration-200 ease-in-out z-5 border-r border-skin-subtle overflow-auto"
 	class:lg:relative={$site.sidebar}
 >
 	<nav
-		class="flex min-h-12 items-center w-full gap-2 border-b border-b-skin-subtle"
+		class="flex min-h-10 items-center w-full gap-2 border-b border-b-skin-subtle"
 	>
 		<Icon
 			name="i-rokkit:action-cross"
@@ -32,10 +32,10 @@
 			on:click={() => ($site.sidebar = false)}
 		/>
 		<input
-			type="text"
+			type="search"
 			placeholder="search"
 			bind:value={search}
-			class="w-full h-full bg-transparent border-none focus:outline-none border-box"
+			class="embedded"
 		/>
 	</nav>
 	<slot />
