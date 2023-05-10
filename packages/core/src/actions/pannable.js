c9e13e569a91f61ba1b2b9cd097d46cab8a398f7
Jerry Thomas <me@jerrythomas.name>
2023-05-10 14:18:48 +0530

    Added support for touch drag in pannable action

--- packages/core/src/actions/pannable.js
+++ packages/core/src/actions/pannable.js
@@ -1,3 +1,4 @@
+// pannable.js
 /**
  * Handle drag and move events
  *
@@ -9,8 +10,8 @@ export function pannable(node) {
 	let y
 
 	function track(event, name, delta = {}) {
-		x = event.clientX
-		y = event.clientY
+		x = event.clientX || event.touches[0].clientX
+		y = event.clientY || event.touches[0].clientY
 		event.stopPropagation()
 		event.preventDefault()
 		node.dispatchEvent(
@@ -24,11 +25,13 @@ export function pannable(node) {
 		track(event, 'panstart')
 		window.addEventListener('mousemove', handleMousemove)
 		window.addEventListener('mouseup', handleMouseup)
+		window.addEventListener('touchmove', handleMousemove, { passive: false })
+		window.addEventListener('touchend', handleMouseup)
 	}
 
 	function handleMousemove(event) {
-		const dx = event.clientX - x
-		const dy = event.clientY - y
+		const dx = (event.clientX || event.touches[0].clientX) - x
+		const dy = (event.clientY || event.touches[0].clientY) - y
 
 		track(event, 'panmove', { dx, dy })
 	}
@@ -38,13 +41,17 @@ export function pannable(node) {
 
 		window.removeEventListener('mousemove', handleMousemove)
 		window.removeEventListener('mouseup', handleMouseup)
+		window.removeEventListener('touchmove', handleMousemove)
+		window.removeEventListener('touchend', handleMouseup)
 	}
 
 	node.addEventListener('mousedown', handleMousedown)
+	node.addEventListener('touchstart', handleMousedown, { passive: false })
 
 	return {
 		destroy() {
 			node.removeEventListener('mousedown', handleMousedown)
+			node.removeEventListener('touchstart', handleMousedown)
 		}
 	}
 }
