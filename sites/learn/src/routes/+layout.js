32f785dd3e9f1345901eb214a7c8f2bbf55cb416
Jerry Thomas <me@jerrythomas.name>
2023-07-20 17:32:09 +0530

    feat: added script to autogenerate entries for stories. modified root layout to perform assimilation

--- sites/learn/src/routes/+layout.js
+++ sites/learn/src/routes/+layout.js
@@ -1,5 +1,8 @@
+import { guide } from '$lib'
+
 /** @type {import('./$types').LayoutLoad} */
-export function load() {
+export async function load() {
+	guide.assimilate()
 	return {
 		// eslint-disable-next-line no-undef
 		version: __APP_VERSION__
