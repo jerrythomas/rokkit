fb6556fe4205d696d02e516f158c8397671b401f
Jerry Thomas <me@jerrythomas.name>
2023-07-24 21:11:05 +0530

    fix: resolved issue with fs & path being imported due to shared functions in tutorial library

---
+++ packages/tutorial/spec/utils.spec.js
@@ -0,0 +1,89 @@
+import { describe, it, expect } from 'vitest'
+import { folderHierarchy, getSequenceAndKey } from '../src/utils'
+
+describe('utils', () => {
+	describe('getSequenceAndKey', () => {
+		it('should return the sequence and key from the given text', () => {
+			const input = '01 - Introduction'
+			const expected = { sequence: 1, key: 'Introduction' }
+			expect(getSequenceAndKey(input)).toEqual(expected)
+		})
+
+		it('should return null if the input does not match the pattern', () => {
+			const input = 'Invalid Text'
+			expect(getSequenceAndKey(input)).toBeNull()
+		})
+	})
+	describe('folderHierarchy', () => {
+		it('should convert an array of files without folders as an array', () => {
+			let files = [
+				{ path: '', name: 'x.md', content: '...' },
+				{ path: '', name: 'a.js', content: '...' }
+			]
+			expect(folderHierarchy(files)).toEqual([
+				{ path: '', name: 'x.md', content: '...' },
+				{ path: '', name: 'a.js', content: '...' }
+			])
+		})
+		it('should convert an array of files without folders as an array', () => {
+			let files = [
+				{ path: 'a', name: 'x.md', content: '...' },
+				{ path: '', name: 'a.js', content: '...' }
+			]
+			expect(folderHierarchy(files)).toEqual([
+				{
+					path: '',
+					name: 'a',
+					type: 'folder',
+					children: [{ path: 'a', name: 'x.md', content: '...' }]
+				},
+				{ path: '', name: 'a.js', content: '...' }
+			])
+		})
+		it('should convert an array of files with path into a nested array', () => {
+			let files = [
+				{ path: 'a/b/c', name: 'x.md', content: '...' },
+				{ path: 'a/b/c', name: 'a.js', content: '...' },
+				{ path: 'a/b', name: 'x.json', content: '...' }
+			]
+			expect(folderHierarchy(files)).toEqual([
+				{
+					name: 'a',
+					path: '',
+					type: 'folder',
+					children: [
+						{
+							name: 'b',
+							path: 'a',
+							type: 'folder',
+							children: [
+								{
+									name: 'c',
+									path: 'a/b',
+									type: 'folder',
+									children: [
+										{
+											path: 'a/b/c',
+											name: 'x.md',
+											content: '...'
+										},
+										{
+											path: 'a/b/c',
+											name: 'a.js',
+											content: '...'
+										}
+									]
+								},
+								{
+									path: 'a/b',
+									name: 'x.json',
+									content: '...'
+								}
+							]
+						}
+					]
+				}
+			])
+		})
+	})
+})
--- packages/tutorial/src/files.js
+++ packages/tutorial/src/files.js
@@ -6,7 +6,7 @@ import path from 'path'
  * @param {string} folderPath - The folder to search in.
  * @param {RegExp} pattern - The pattern to match the file names.
  * @param {string} [dir=''] - The current directory, used for recursion.
- * @returns {Promise<Array<import('./types.js').FileMetadata>>} - An array of matched files.
+ * @returns {Promise<Array<import('../../rokkit/packages/tutorial/src/types.js').FileMetadata>>} - An array of matched files.
  */
 export async function getFiles(folderPath, pattern = null, dir = '') {
 	const entries = await fs.promises.readdir(path.join(folderPath, dir), {
@@ -30,35 +30,6 @@ export async function getFiles(folderPath, pattern = null, dir = '') {
 	)
 
 	return Array.prototype.concat(...files).filter(Boolean)
-}
-
-/**
- * Converts a flat array of file metadata into a nested array of file metadata.
- *
- * @param {Array<import('./types.js').FileMetadata>} files - Flat Array of file metadata
- * @returns {Array<import('./types.js').FileMetadata>} - Nested Array of file metadata
- */
-export function folderHierarchy(files) {
-	const result = files.reduce((acc, file) => {
-		const parts = file.path.split('/')
-		let current = acc
-		parts.forEach((part, index) => {
-			let child = current.find((item) => item.name === part)
-			if (!child && part != '') {
-				child = {
-					name: part,
-					path: parts.slice(0, index).join('/'),
-					type: 'folder',
-					children: []
-				}
-				current.push(child)
-			}
-			if (part != '') current = child.children
-		})
-		current.push(file)
-		return acc
-	}, [])
-	return result
 }
 
 // export function filesToNestedArray(files) {
