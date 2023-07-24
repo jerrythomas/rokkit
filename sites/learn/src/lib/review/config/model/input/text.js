b5b104ed85f2aad59ae247f50992232a827b8d18
Jerry Thomas <me@jerrythomas.name>
2023-07-24 18:48:38 +0530

    chore: increased memory size to resolve action run issue.

--- sites/learn/src/lib/review/config/model/input/text.js
+++
@@ -1,9 +0,0 @@
-export default [
-	{ name: 'readOnly', type: 'boolean' },
-	{ name: 'disabled', type: 'boolean' },
-	{ name: 'placeholder', type: 'string' },
-	{ name: 'value', type: 'string' },
-	{ name: 'size', type: 'integer' },
-	{ name: 'pattern', type: 'string' },
-	{ name: 'autocomplete', type: 'list', items: ['on', 'off'] }
-]
