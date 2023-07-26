8e769216f41389fd41cea314855de81b6e0fe174
Jerry Thomas <me@jerrythomas.name>
2023-07-26 13:44:37 +0530

    fix: corrected examples for DropDown and the theme associated with it

--- sites/learn/src/stories/04-organisms/08-drop-down/01-properties/src/App.svelte
+++ sites/learn/src/stories/04-organisms/08-drop-down/01-properties/src/App.svelte
@@ -1,9 +1,9 @@
 <script>
 	import { DropDown } from '@rokkit/organisms'
 
-	let items = ['Alpha ', 'Beta', 'Gamma']
+	let options = ['Alpha ', 'Beta', 'Gamma']
 	let value
 </script>
 
-<DropDown {items} bind:value title="Select a value" />
+<DropDown {options} bind:value title="Select a value" />
 <p>Selected Value: <b>{value}</b></p>
--- sites/learn/src/stories/04-organisms/08-drop-down/02-fields/src/App.svelte
+++ sites/learn/src/stories/04-organisms/08-drop-down/02-fields/src/App.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { DropDown } from '@rokkit/organisms'
-	import { items } from './data.js'
+	import { options } from './data.js'
 
 	let fields = { text: 'name', image: 'photo' }
 	let value
 </script>
 
-<DropDown {items} {fields} bind:value title="Dropdown" />
+<DropDown {options} {fields} bind:value title="Dropdown" />
 
 <p>Selected Value</p>
 <pre>{JSON.stringify(value, null, 2)}</pre>
