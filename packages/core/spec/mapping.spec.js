df027e47a6362d3c4584c705eafbd77a2c5e74fa
Jerry Thomas <me@jerrythomas.name>
2023-07-30 17:34:51 +0530

    feat: added utility functions to traverse a nested list

--- packages/core/spec/mapping.spec.js
+++ packages/core/spec/mapping.spec.js
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest'
+import items from './fixtures/nested-items.json'
 import {
 	getComponent,
 	getIcon,
@@ -8,8 +9,11 @@ import {
 	isExpanded,
 	isNested,
 	findItemByValue,
+	findLastVisibleChild,
+	findNearestItemBefore,
+	findNearestItemAfter,
 	getItemByIndexArray
-} from '../src/mapping.js'
+} from '../src/mapping'
 
 describe('mapping', () => {
 	const fields = {
@@ -182,7 +186,7 @@ describe('mapping', () => {
 			const fields = { children: 'children' }
 			const value = 'd'
 
-			const result = findItemByValue(items, fields, value)
+			const result = findItemByValue(value, items, fields)
 
 			expect(result).toEqual({
 				fields,
@@ -202,7 +206,7 @@ describe('mapping', () => {
 			const fields = { children: 'children' }
 			const value = 'f'
 
-			const result = findItemByValue(items, fields, value)
+			const result = findItemByValue(value, items, fields)
 
 			expect(result).toBeNull()
 		})
@@ -227,6 +231,173 @@ describe('mapping', () => {
 			const indexArray = [0, 0, 0, 0]
 			const item = getItemByIndexArray(indexArray, items, fields)
 			expect(item).toBeFalsy()
+		})
+	})
+
+	describe('findLastVisibleChild', () => {
+		const fields = { text: 'text', children: 'nodes', isOpen: 'is_open' }
+
+		it('should return the item itself if it is a leaf', () => {
+			const item = items[2]
+			const position = [2]
+			const result = findLastVisibleChild(item, position, fields)
+			expect(result).toEqual({ item, position, fields })
+		})
+
+		it('should return the last child if the item is not a leaf and is open', () => {
+			const item = items[0]
+			const result = findLastVisibleChild(item, [0], fields)
+			expect(result).toEqual({ item: item.nodes[1], position: [0, 2], fields })
+		})
+
+		it('should return the last visible grandchild intermediate nodes are open', () => {
+			const item = items[0].nodes[0]
+			const result = findLastVisibleChild(item, [0, 0], fields)
+			expect(result).toEqual({
+				item: item.nodes[1].nodes[0],
+				position: [0, 0, 1, 0],
+				fields
+			})
+		})
+
+		it('should return the item itself if it is not a leaf and is not open', () => {
+			const item = items[1]
+			const result = findLastVisibleChild(item, [1], fields)
+			expect(result).toEqual({ item, position: [1], fields })
+		})
+	})
+
+	describe('findNearestItemBefore', () => {
+		const fields = { text: 'text', children: 'nodes', isOpen: 'is_open' }
+
+		it('should return null if there are no items', () => {
+			const current = { item: null, fields, position: [] }
+			const result = findNearestItemBefore(current.position, [], fields)
+			expect(result).toBeNull()
+		})
+
+		it('should return the previous sibling if it is a leaf', () => {
+			const current = {
+				item: items[0].nodes[0].nodes[1],
+				fields,
+				position: [0, 0, 1]
+			}
+			const result = findNearestItemBefore(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[0].nodes[0],
+				fields,
+				position: [0, 0, 0]
+			})
+		})
+
+		it('should return the parent if the previous sibling is not a leaf and is not open', () => {
+			const current = {
+				item: items[0].nodes[0].nodes[1].nodes[0],
+				fields,
+				position: [0, 0, 1, 0]
+			}
+			const result = findNearestItemBefore(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[0].nodes[1],
+				fields,
+				position: [0, 0, 1]
+			})
+		})
+
+		it('should return the last visible child if the previous sibling is not a leaf and is open', () => {
+			const current = { item: items[0].nodes[1], fields, position: [0, 1] }
+			const result = findNearestItemBefore(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[0].nodes[1].nodes[0],
+				fields,
+				position: [0, 0, 1, 0]
+			})
+		})
+
+		it('should return the previous sibling if it is not a leaf and is closed', () => {
+			const current = { item: items[2], fields, position: [2] }
+			const result = findNearestItemBefore(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[1],
+				fields,
+				position: [1]
+			})
+		})
+	})
+
+	describe('findNearestItemAfter', () => {
+		const fields = { text: 'text', children: 'nodes', isOpen: 'is_open' }
+
+		it('should return null if there are no items', () => {
+			const current = { item: null, fields, position: [] }
+			const result = findNearestItemAfter(current.position, [], fields)
+			expect(result).toBeNull()
+		})
+
+		it('should return first child if it is open', () => {
+			const current = {
+				item: items[0],
+				fields,
+				position: [0]
+			}
+			const result = findNearestItemAfter(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[0],
+				fields,
+				position: [0, 0]
+			})
+		})
+
+		it('should return the next sibling if current is a leaf', () => {
+			const current = {
+				item: items[0].nodes[1],
+				fields,
+				position: [0, 1]
+			}
+			const result = findNearestItemAfter(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[2],
+				fields,
+				position: [0, 2]
+			})
+		})
+
+		it('should return the next sibling if current is closed', () => {
+			const current = {
+				item: items[1],
+				fields,
+				position: [1]
+			}
+			const result = findNearestItemAfter(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[2],
+				fields,
+				position: [2]
+			})
+		})
+
+		it('should return null if current is last', () => {
+			const current = { item: items[2], fields, position: [2] }
+			let result = findNearestItemAfter(current.position, items, fields)
+			expect(result).toBeNull()
+
+			const nodes = [items[0].nodes[0]]
+			result = findNearestItemAfter([0, 1, 0], nodes, fields)
+			expect(result).toBeNull()
+		})
+
+		it('should return the nearest visible parent', () => {
+			const current = {
+				item: items[0].nodes[0].nodes[1].nodes[0],
+				fields,
+				position: [0, 0, 1, 0]
+			}
+			const result = findNearestItemAfter(current.position, items, fields)
+			expect(result).toEqual({
+				item: items[0].nodes[1],
+				fields,
+				position: [0, 1]
+			})
 		})
 	})
 })
