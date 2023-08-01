b2092f8053edc02800d89e5962ae152e91e06be4
Jerry Thomas <me@jerrythomas.name>
2023-08-01 21:13:02 +0530

    fix: revert unnecessary separation into common function. readability is better with separate functions.

--- packages/actions/src/lib/event-manager.js
+++ packages/actions/src/lib/event-manager.js
@@ -4,22 +4,22 @@
 export function EventManager(element, handlers = {}) {
 	let listening = false
 
-	function toggle() {
-		const action = listening ? 'removeEventListener' : 'addEventListener'
-		Object.entries(handlers).forEach(([event, handler]) =>
-			element[action](event, handler)
-		)
-		listening = !listening
-	}
-
 	function activate() {
-		if (!listening) toggle()
+		if (!listening) {
+			Object.entries(handlers).forEach(([event, handler]) =>
+				element.addEventListener(event, handler)
+			)
+			listening = true
+		}
 	}
-
 	function destroy() {
-		if (listening) toggle()
+		if (listening) {
+			Object.entries(handlers).forEach(([event, handler]) =>
+				element.removeEventListener(event, handler)
+			)
+			listening = false
+		}
 	}
-
 	function update(enabled, newHandlers = handlers) {
 		if (listening !== enabled || handlers !== newHandlers) {
 			destroy()
