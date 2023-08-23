d4fc4475495563461a9581c35a283c03f0bff6a4
Jerry Thomas <me@jerrythomas.name>
2023-08-23 20:18:55 +0530

    feat: updated wip to test virtual list

--- sites/learn/src/routes/wip/+page.svelte
+++ sites/learn/src/routes/wip/+page.svelte
@@ -1,38 +1,17 @@
 <script>
-	import { Icon } from '@rokkit/atoms'
+	import { VirtualSelect, VirtualList } from '@rokkit/organisms'
+	// import VirtualList from '@sveltejs/svelte-virtual-list'
+	import { Item } from '@rokkit/molecules'
+	const options = Array.from({ length: 50 }).map((_, i) => `Option ${i + 1}`)
+	let start
+	let end
 </script>
 
 <span>
 	<h1>Work in Progress</h1>
-	<p>This page is a work in progress.</p>
 </span>
-<div class="h-full w-full flex flex-col items-center">
-	<span class="flex items-center">
-		<x
-			class="aspect-square h-4 w-4 flex items-center justify-center border rounded-full text-red text-sm"
-		>
-			<i class="text-red i-rokkit:heart-filled" />
-		</x>
-		<x
-			class="aspect-square h-5 w-5 flex items-center justify-center border rounded-full text-base text-red"
-		>
-			<i class="text-red i-rokkit:heart-filled" />
-		</x>
-		<x
-			class="aspect-square h-6 w-6 flex items-center justify-center border rounded-full text-lg text-red"
-		>
-			<i class="text-red i-rokkit:heart-filled" />
-		</x>
-		<x
-			class="aspect-square h-10 w-10 flex items-center justify-center border rounded-full text-2xl text-red"
-		>
-			<i class="text-red i-rokkit:heart-filled" />
-		</x>
-		<Icon
-			name="i-rokkit:heart-filled"
-			size="small"
-			role="button"
-			class="border rounded-full"
-		/>
-	</span>
-</div>
+
+<VirtualList items={options} limit={5} bind:start bind:end></VirtualList>
+
+{start}-{end}
+<!-- <VirtualList items={options} /> -->
