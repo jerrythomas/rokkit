5e4de4773d6c7587b981613fcafdc5dec4d9aef9
Jerry Thomas <me@jerrythomas.name>
2023-10-27 14:08:50 +0530

    feat: added multiple color themes

---
+++ sites/learn/src/lib/Carousel.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { getText } from '@rokkit/core'
+	import { get } from 'svelte/store'
+
+	export let items
+	export let value
+	export let fields
+</script>
+
+<carousel>
+	{#each items as item}
+		{@const text = getText(item, fields)}
+		{@const image = item[fields.image]}
+		<carousel-item {item} />
+	{/each}
+</carousel>
