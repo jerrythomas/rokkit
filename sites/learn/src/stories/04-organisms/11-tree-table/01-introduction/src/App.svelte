06a40af18512bd7297af949d60cfe226d0302935
Jerry Thomas <me@jerrythomas.name>
2024-01-16 16:35:51 +0530

    chore: resolve coverage issues

---
+++ sites/learn/src/stories/04-organisms/11-tree-table/01-introduction/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { TreeTable } from '@rokkit/organisms'
+  import { data } from './data.js'
+
+  let columns = [{key:'scope'},{key: 'level'}, {key: 'name'}, {key:'unitsSold'}]
+</script>
+
+<TreeTable {data} {columns} />
