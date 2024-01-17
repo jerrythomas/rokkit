22ff9e2ccc87b2af80c99b4c60a22f45987226d5
Jerry Thomas <me@jerrythomas.name>
2024-01-17 11:33:30 +0530

    feat: added multiselect feature to TreeTable and externalized the styles

--- packages/organisms/src/TreeTable.svelte
+++ packages/organisms/src/TreeTable.svelte
@@ -1,18 +1,65 @@
 <script>
-	import { pick } from 'ramda'
+	import { createEventDispatcher } from 'svelte'
+  import { pick, omit } from 'ramda'
 	import { defaultFields } from '@rokkit/core'
 	import { Connector, Icon } from '@rokkit/atoms'
 	import { Item } from '@rokkit/molecules'
+
+	const dispatch = createEventDispatcher()
 
 	let className = ''
 	export { className as class }
 	export let data = []
 	export let columns = []
 	export let striped = true
+	export let value = null
+	export let multiselect = false
 
 	let hiddenPaths = []
+	let currentItem = null
 
+	function handleItemClick(event, item) {
+		if (item._isParent)
+		   toggle(item)
+		else {
+			currentItem = item
+			value = getValue(item)
+		  dispatch('click', value)
 
+			if (event.metaKey) toggleSelection(event, item)
+		}
+	}
+  function getValue(item) {
+		return omit(['_levels', '_isParent', '_isExpanded', '_depth', '_path', '_selected'], item)
+	}
+	function toggleSelection(e,item) {
+		e.stopPropagation()
+		e.preventDefault()
+		if (item._selected != 'checked')
+		   item._selected = 'checked'
+		else
+		   item._selected = 'unchecked'
+		// item._selected = !item._selected
+		if (item._isParent) {
+			data.filter((i) => i[nestedColumn.key].startsWith(item[nestedColumn.key]+'/')).forEach((i) => (i._selected = item._selected))
+		} else {
+			// find parent and update its state
+			const parents = data.filter((i) => item[nestedColumn.key].startsWith(i[nestedColumn.key]+'/') && i._isParent)
+			parents.map((p) => {
+				const children = data.filter((i) => i[nestedColumn.key].startsWith(p[nestedColumn.key]+'/'))
+				const selectedChildren = children.filter((i) => i._selected === 'checked')
+
+				if (selectedChildren.length === children.length)
+					p._selected = 'checked'
+				else if (selectedChildren.length === 0)
+					p._selected = 'unchecked'
+				else
+					p._selected = 'unknown'
+			})
+		}
+    filtered = [...data.filter(isVisible)]
+		dispatch('select', data.filter((i) => i._selected).map(getValue))
+	}
 	function toggle(item) {
 		if (nestedColumn === undefined) return
 
@@ -33,28 +80,64 @@
 		return !hiddenPaths.some((i) => item[nestedColumn.key].startsWith(i))
 	}
 
+	function addMultiSelectColumn(multiselect) {
+		if (multiselect) {
+			if (columns.some((col) => col.key === '_selected')) return
+      columns = [{ key: '_selected',label:'', width: '3rem' }, ...columns]
+			data.forEach((item) => {
+				item._selected = 'unchecked'
+			})
+		} else {
+			columns = [...columns.filter((col) => col.key !== '_selected')]
+		}
+	}
+	// function getCheckedState(item){
+  //    if (item._isParent) {
+  //      const children = data.filter((i) => i[nestedColumn.key].startsWith(item[nestedColumn.key]))
+	// 		 const selectedChildren = children.filter((i) => i._selected)
+	// 		 if (selectedChildren.length === 0) return 'unchecked'
+	// 		 if (selectedChildren.length === children.length) return 'checked'
+	// 		 return 'unknown'
+	// 	 } else {
+	// 		 return item._selected ? 'checked' : 'unchecked'
+	// 	 }
+	// }
+
 	$: filtered = data
+	$: addMultiSelectColumn(multiselect)
 	$: sizes = columns.map((col) => col.width ?? '1fr').join(' ')
 	$: nestedColumn = columns.find((col) => col.path)
 </script>
 
-<tree-table class="flex flex-col h-full w-full overflow-auto {className}" style:--sizes={sizes}>
-	<table class="flex flex-col mb-auto overflow-y-hidden">
+<tree-table class="{className}" style:--sizes={sizes}>
+	<table>
 		<thead>
-			<tr class="grid gap-1px uppercase">
+			<tr>
 				{#each columns as col}
-					<th class="bg-neutral-400">{col.label}</th>
+					<th>{col.label}</th>
 				{/each}
 			</tr>
 		</thead>
-		<tbody class="flex flex-col gap-1px overflow-y-auto">
+		<tbody>
 			{#each filtered as item, index}
 				{@const even = striped && index % 2 === 0}
-				<tr class="grid gap-1px" class:even>
-					{#each columns as col}
+				<tr class:even
+						class:cursor-pointer={!item._isParent}
+						aria-current={currentItem === item}
+						on:click|preventDefault|stopPropagation={(e) => handleItemClick(e,item)}>
+					{#each columns as col, index}
 						{@const value = { ...pick(['icon'], col), ...item }}
 						{@const fields = { ...defaultFields, text: col.key, ...col.fields }}
-						<td class="flex gap-2 bg-neutral-subtle">
+						<td>
+							{#if multiselect && index === 0}
+							 <!-- {#if !item._isParent} -->
+							<Icon
+								name={'checkbox-' + item._selected}
+								class="small cursor-pointer"
+							  on:click={(e) => toggleSelection(e,item)}
+						  />
+						<!-- {/if} -->
+							{:else}
 							{#if col.path}
 								{#each item._levels.slice(0, -1) as _}
 									<Connector type="empty" />
@@ -63,13 +146,13 @@
 									<Icon
 										name={item._isExpanded ? 'node-opened' : 'node-closed'}
 										class="small cursor-pointer"
-										on:click={() => toggle(item)}
 									/>
 								{:else if item._depth > 0}
 									<Connector type="empty" />
 								{/if}
 							{/if}
 							<Item {value} {fields} />
+							{/if}
 						</td>
 					{/each}
 				</tr>
@@ -82,14 +165,12 @@
 	tr {
 		grid-template-columns: var(--sizes);
 	}
-	tr.even > td {
-		@apply bg-neutral-base;
-	}
-	th,
+
+	/* th,
 	td {
 		@apply px-4 py-3;
 	}
 	td :global(icon) {
 		@apply text-secondary-700;
-	}
+	} */
 </style>
