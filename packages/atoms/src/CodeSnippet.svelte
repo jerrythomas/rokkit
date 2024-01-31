bdbd1d2bae43dc6a3c10134192cc921c39843c6b
Jerry Thomas <me@jerrythomas.name>
2024-01-31 14:21:00 +0530

    chore: resolve prism-svelte dependency issue

--- packages/atoms/src/CodeSnippet.svelte
+++ packages/atoms/src/CodeSnippet.svelte
@@ -1,12 +1,13 @@
 <script>
-	import * as prism from 'prismjs'
+	import * as Prism from 'prismjs'
+  import 'prism-svelte'
 
 	let className = ''
 	export { className as class }
 	export let code
 	export let language
 
-	$: parsed = prism.highlight(code, prism.languages[language], language)
+	$: parsed = Prism.highlight(code, Prism.languages[language], language)
 </script>
 
 <pre class="language-{language} {className}"><code>{@html parsed}</code></pre>
