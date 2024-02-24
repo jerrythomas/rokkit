dce03de8aa4388c7199ed04da5a699be929af242
Jerry Thomas <me@jerrythomas.name>
2024-02-24 12:34:21 +0530

    feat: added a ui package which includes all components from atoms, molecules and organisms

---
+++ packages/ui/README.md
@@ -0,0 +1,3 @@
+# @rokkit/ui
+
+Makes all ui components available from atoms/molecules/organisms/templates/pages in one place.
---
+++ packages/ui/src/index.js
@@ -0,0 +1,4 @@
+export * from '@rokkit/atoms'
+export * from '@rokkit/molecules'
+export * from '@rokkit/organisms'
+export * from '@rokkit/layout'
