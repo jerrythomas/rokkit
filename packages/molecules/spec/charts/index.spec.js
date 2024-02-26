b647958fe8b7f6205eab08fb6410baa4cd4f5b4a
Jerry Thomas <me@jerrythomas.name>
2024-02-26 13:49:22 +0530

    chore: remove all components related to charts

--- packages/molecules/spec/charts/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest'
-import * as components from '../../src/charts'
-
-describe('components', () => {
-	it('should contain all exported components', () => {
-		expect(Object.keys(components)).toEqual(['DefinePatterns', 'Symbol'])
-	})
-})
--- packages/molecules/src/charts/index.js
+++
@@ -1,2 +0,0 @@
-export { default as DefinePatterns } from './DefinePatterns.svelte'
-export { default as Symbol } from './Symbol.svelte'
