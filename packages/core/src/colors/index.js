ba6d0e2d63fcc5f52bb2873c66239db882afe4e9
Jerry Thomas <me@jerrythomas.name>
2024-03-07 11:24:49 +0530

    fix: restructured colors and deriveColumnMetadata for maintainability

--- packages/core/src/colors/index.js
+++ packages/core/src/colors/index.js
@@ -1,4 +1,5 @@
-import { defaultTailwindColors } from './tailwind'
+import defaultTailwindColors from './tailwind.json'
+
 export const shades = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 950]
 export const defaultPalette = [
 	'neutral',
