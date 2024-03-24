2b1c66117055f1d1065324ffb2a4e328346d7340
Jerry Thomas <me@jerrythomas.name>
2024-03-24 18:21:56 +0530

    fix: added background color for sidebar

--- sites/learn/src/routes/(learn)/Sidebar.svelte
+++ sites/learn/src/routes/(learn)/Sidebar.svelte
@@ -6,7 +6,7 @@
 	 * @type {HTMLElement}
 	 */
 	let sidebar
-	let search
+	let search = ''
 
 	function toggle(site) {
 		sidebar.classList.add('duration-200')
@@ -22,7 +22,7 @@
 
 <aside
 	bind:this={sidebar}
-	class="-translate-x-full absolute left-0 top-0 w-full flex flex-shrink-0 flex-col gap-4 md:w-1/2 lg:w-80"
+	class="-translate-x-full absolute left-0 top-0 w-full flex flex-shrink-0 flex-col md:w-1/2 lg:w-80"
 	class:lg:relative={$site.sidebar}
 >
 	<nav class="h-10 w-full flex items-center gap-2 border-b border-b-neutral-inset">
@@ -40,7 +40,7 @@
 <style>
 	aside {
 		@apply h-full z-5 overflow-auto text-xs;
-		@apply border-r border-neutral-inset;
+		@apply border-r border-neutral-inset bg-neutral-base;
 		@apply transform ease-in-out;
 	}
 </style>
