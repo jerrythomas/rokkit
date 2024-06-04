8a7b1c5ba958722a8f5fb7aa10ca4cb5d97e0ff5
Jerry Thomas <me@jerrythomas.name>
2024-06-04 14:19:19 +0530

    chore: remove duplicate parser from core.

--- packages/core/spec/utils.spec.js
+++ packages/core/spec/utils.spec.js
@@ -5,6 +5,7 @@ describe('utils', () => {
 	describe('noop', () => {
 		it('should be a function', () => {
 			expect(typeof noop).toBe('function')
+			expect(noop()).toBeUndefined()
 		})
 	})
 	describe('id', () => {
