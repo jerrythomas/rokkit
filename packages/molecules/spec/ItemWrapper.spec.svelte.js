c9b319e2784704d3816feaf7ad655546cb9fa59a
Jerry Thomas <me@jerrythomas.name>
2024-11-18 16:22:57 -0600

    fix: updated item wrapper and corresponding test to use svelte 5

--- packages/molecules/spec/ItemWrapper.spec.svelte.js
+++ packages/molecules/spec/ItemWrapper.spec.svelte.js
@@ -1,11 +1,8 @@
 import { describe, expect, it, beforeEach, vi } from 'vitest'
 import { cleanup, render, fireEvent } from '@testing-library/svelte'
-import { toHaveBeenDispatchedWith } from 'validators'
 import { tick } from 'svelte'
 import ItemWrapper from '../src/ItemWrapper.svelte'
 import Custom from './mocks/Custom.svelte'
-
-expect.extend({ toHaveBeenDispatchedWith })
 
 describe('ItemWrapper.svelte', () => {
 	beforeEach(() => cleanup())
@@ -17,38 +14,41 @@ describe('ItemWrapper.svelte', () => {
 	})
 
 	it('should handle value change', async () => {
-		const { container, component } = render(ItemWrapper, { props: { value: 'hello' } })
+		const props = $state({ value: 'hello' })
+		const { container, component } = render(ItemWrapper, { props })
 		expect(container).toBeTruthy()
 		expect(container).toMatchSnapshot()
 
-		// handle value change
-		//   setProperties(component, { props: { value: 'hello' } })
-		// await tick()
-		// const text = container.querySelector('p')
-		// expect(text).toBeTruthy()
-		// expect(text.textContent).toBe('hello')
+		props.value = 'hello'
+		await tick()
+		const text = container.querySelector('p')
+		expect(text).toBeTruthy()
+		expect(text.textContent).toBe('hello')
 	})
 
-	// it('should handle change for removable property', async () => {
-	// 	const { container, component } = render(ItemWrapper, { value: 'hello' })
-	// 	expect(container).toMatchSnapshot()
+	it('should handle change for removable property', async () => {
+		const props = $state({ value: 'hello' })
+		const { container, component } = render(ItemWrapper, { props })
+		expect(container).toMatchSnapshot()
 
-	// 	setProperties(component,{ removable: true })
-	// 	await tick()
-	// 	expect(container).toMatchSnapshot()
-	// })
+		props.removable = true
+		await tick()
+		expect(container).toMatchSnapshot()
+	})
 
 	it('should handle class change', async () => {
-		const { container, component } = render(ItemWrapper, { props: { value: 'hello' } })
+		const props = $state({ value: 'hello' })
+		const { container } = render(ItemWrapper, { props })
 		let wrapper = container.querySelector('wrap-item')
 		expect(wrapper).toBeTruthy()
 		expect(Array.from(wrapper.classList)).not.toContain('item')
 
 		// setProperties(component, { class: 'item' })
-		// await tick()
-		// wrapper = container.querySelector('wrap-item')
-		// expect(wrapper).toBeTruthy()
-		// expect(Array.from(wrapper.classList)).toContain('item')
+		props.class = 'item'
+		await tick()
+		wrapper = container.querySelector('wrap-item')
+		expect(wrapper).toBeTruthy()
+		expect(Array.from(wrapper.classList)).toContain('item')
 	})
 
 	it('should render using field mapping', () => {
@@ -69,20 +69,20 @@ describe('ItemWrapper.svelte', () => {
 	it('should render with removable prop', async () => {
 		const removeEvent = vi.fn()
 
-		const { component, container, getByRole } = render(ItemWrapper, {
+		const { container, getByRole } = render(ItemWrapper, {
 			props: {
 				value: 'Test',
-				removable: true
-			},
-			events: { remove: removeEvent }
+				removable: true,
+				onremove: removeEvent
+			}
+			// events: { remove: removeEvent }
 		})
 		expect(container).toBeTruthy()
 		expect(container).toMatchSnapshot()
 
-		const removeIcon = getByRole('button', { name: 'Remove' })
-		// component.$on('remove', removeEvent)
+		const removeIcon = getByRole('button', { 'aria-label': 'Remove' })
 		await fireEvent.click(removeIcon)
-		expect(removeEvent).toHaveBeenDispatchedWith('Test')
+		expect(removeEvent).toHaveBeenCalledWith('Test')
 	})
 
 	it('should render with selected prop', () => {
@@ -145,20 +145,16 @@ describe('ItemWrapper.svelte', () => {
 	})
 
 	it('should render removable with default icon when null', async () => {
+		const props = $state({ value: { text: 'Test' }, icons: null, removable: true })
 		const { container, component } = render(ItemWrapper, {
-			props: {
-				value: { text: 'Test' },
-				icons: null,
-				removable: true
-			}
+			props
 		})
 
 		expect(container).toBeTruthy()
 		expect(container).toMatchSnapshot()
-
-		// setProperties(component, { icons: { remove: 'close' } })
-		// await tick()
-		// expect(container).toMatchSnapshot()
+		props.icons = { remove: 'close' }
+		await tick()
+		expect(container).toMatchSnapshot()
 	})
 
 	it('should render object configured component', () => {
@@ -174,20 +170,18 @@ describe('ItemWrapper.svelte', () => {
 	})
 
 	it('should update when value changes', async () => {
-		const { container, component } = render(ItemWrapper, {
-			props: {
-				value: {
-					text: 'hello'
-				}
-			}
+		const props = $state({ value: { text: 'hello' } })
+		const { container } = render(ItemWrapper, {
+			props
 		})
 		expect(container).toBeTruthy()
 		const text = container.querySelector('p')
 		expect(container).toMatchSnapshot()
 		expect(text.textContent).toBe('hello')
 
-		// setProperties(component, { value: { text: 'world', icon: 'world' } })
-		// await tick()
-		// expect(text.textContent).toBe('world')
+		props.value = { text: 'world', icon: 'world' }
+
+		await tick()
+		expect(text.textContent).toBe('world')
 	})
 })
