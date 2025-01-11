3fe854160bb50e4214ac1defb01e6c4f09c1b44a
Jerry Thomas <me@jerrythomas.name>
2025-01-10 22:40:37 -0600

    chore: release v1.0.0-next.103

--- packages/atoms/spec/index.spec.js
+++ packages/atoms/spec/index.spec.js
@@ -4,6 +4,6 @@ import * as components from '../src/index.js'
 
 describe('components', () => {
 	it('should contain all exported components', () => {
-		expect(Object.keys(components)).toEqual(['Icon'])
+		expect(Object.keys(components)).toEqual(['Icon', 'Toggle', 'Switch', 'Item', 'Pill'])
 	})
 })
