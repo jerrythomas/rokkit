4b2692753e4ed9f9d14c5151450b13847587f0c8
Jerry Thomas <me@jerrythomas.name>
2025-02-25 12:24:32 -0600

    chore: add adapter vercel

--- sites/learn/svelte.config.js
+++ sites/learn/svelte.config.js
@@ -1,5 +1,6 @@
 import { mdsvex } from 'mdsvex'
-import adapter from '@sveltejs/adapter-auto'
+// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,9 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs22.x'
+		})
 	},
 
 	preprocess: [mdsvex()],
