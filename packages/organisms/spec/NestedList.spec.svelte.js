145ecebc11adb41426a0b7908d16bd9c52eb69f6
Jerry Thomas <me@jerrythomas.name>
2025-02-26 18:02:30 -0600

    feat: migrated the Nested List

---
+++ packages/organisms/spec/NestedList.spec.svelte.js
@@ -0,0 +1,30 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { render, cleanup } from '@testing-library/svelte'
+import NestedList from '../src/NestedList.svelte'
+import { flushSync } from 'svelte'
+
+describe('NestedList', () => {
+	const items = [
+		{ text: 'Item 1', _open: true, children: [{ text: 'Child 1' }, { text: 'Child 2' }] },
+		{
+			text: 'Item 2',
+			_open: true,
+			children: [
+				{ text: 'Child 3', _open: true, children: [{ text: 'Grandchild 1' }] },
+				{ text: 'Child 4' }
+			]
+		}
+	]
+	beforeEach(() => cleanup())
+
+	it('should render nested list', () => {
+		const props = $state({ items })
+		const { container } = render(NestedList, props)
+		expect(container).toBeTruthy()
+		expect(container).toMatchSnapshot()
+
+		props.class = 'another-class'
+		flushSync()
+		expect(container).toMatchSnapshot()
+	})
+})
