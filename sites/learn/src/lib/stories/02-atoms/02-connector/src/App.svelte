75c2808b4855bd4c142dfe87ac25ddf7340c9dea
Jerry Thomas <me@jerrythomas.name>
2025-02-27 19:01:59 -0600

    feat: added stories for atomic components icon and connector

---
+++ sites/learn/src/lib/stories/02-atoms/02-connector/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Connector } from '@rokkit/ui'
+
+	let variants = [
+		{ title: 'last', props: { type: 'last' } },
+		{ title: 'child', props: { type: 'child' } },
+		{ title: 'sibling', props: { type: 'sibling' } },
+		{ title: 'empty', props: { type: 'empty' } }
+	]
+</script>
+
+<h2>Regular connectors</h2>
+<div class="flex w-60 gap-2 border px-4 py-2">
+	{#each variants as { title, props }}
+		<div class="flex flex-col items-center">
+			<h1 class="flex-start">{title}</h1>
+			<span class="h-8 w-4">
+				<Connector {...props} />
+			</span>
+		</div>
+	{/each}
+</div>
+<h2>With RTL</h2>
+<div class="flex w-60 gap-2 border px-4 py-2">
+	{#each variants as { title, props }}
+		<div class="flex flex-col items-center">
+			<h1 class="flex-start">{title}</h1>
+			<span class="h-8 w-4">
+				<Connector {...props} rtl />
+			</span>
+		</div>
+	{/each}
+</div>
