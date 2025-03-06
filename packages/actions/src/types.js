abe6a26d72cc72d30b717bc7518017aaa096d944
Jerry Thomas <me@jerrythomas.name>
2025-03-06 14:30:08 -0600

    feat: added navigator action and updated the tree navigator wrapper for nested data navigation

--- packages/actions/src/types.js
+++ packages/actions/src/types.js
@@ -8,3 +8,43 @@
 /**
  * @typedef {Object<string, (string[]|RegExp) >} KeyboardConfig
  */
+
+/**
+ * @typedef {'vertical'|'horizontal'} Direction
+ */
+
+/**
+ * @typedef {Object} NavigatorOptions
+ * @property {boolean}   enabled     - Whether the navigator is enabled
+ * @property {Direction} direction   - Whether the navigator is vertical or horizontal
+ * @property {boolean}   multiselect - Whether the navigator supports multiple selections
+ */
+
+/**
+ * @typedef {Object} DataWrapper
+ * @property {Function} moveNext
+ * @property {Function} movePrev
+ * @property {Function} moveFirst
+ * @property {Function} moveLast
+ * @property {Function} expand
+ * @property {Function} collapse
+ * @property {Function} select
+ * @property {Function} toggleExpansion
+ */
+
+/**
+ * @typedef {Object} NavigatorActions
+ * @property {Function} next
+ * @property {Function} prev
+ * @property {Function} first
+ * @property {Function} last
+ * @property {Function} expand
+ * @property {Function} collapse
+ * @property {Function} select
+ */
+
+/**
+ * @typedef {Object} NavigatorConfig
+ * @property {Navigator} wrapper     - Whether the navigator is enabled
+ * @property {NavigatorOptions} options   - Whether the navigator is vertical or horizontal
+ */
