f90f020bb626bde1eea37c18113420bd1f34e077
Jerry Thomas <me@jerrythomas.name>
2026-01-30 13:28:28 -0600

    feat: updated tree root and some stories

--- packages/ui/src/tree/Root.svelte
+++ packages/ui/src/tree/Root.svelte
@@ -1,81 +1,82 @@
 <script>
-  import { createEmitter, defaultFields } from '@rokkit/core'
-  import { navigator } from '@rokkit/actions'
-  import List from './List.svelte'
-  import { NestedController } from '@rokkit/states'
-  import { omit, has } from 'ramda'
+	import { createEmitter, defaultFields } from '@rokkit/core'
+	import { navigator } from '@rokkit/actions'
+	import List from './List.svelte'
+	import { NestedController } from '@rokkit/states'
+	import { omit, has } from 'ramda'
 
-  /**
-   * @typedef {Object} Props
-   * @property {string} [class]
-   * @property {Array<any>} [items]
-   * @property {any} [value]
-   * @property {import('@rokkit/core').FieldMapping} [fields]
-   * @property {Object} [icons]
-   * @property {boolean} [autoCloseSiblings=false]
-   * @property {boolean} [multiselect=false]
-   * @property {Function} [header]
-   * @property {Function} [footer]
-   * @property {Function} [empty]
-   * @property {Function} [stub]
-   */
+	/**
+	 * @typedef {Object} Props
+	 * @property {string} [class]
+	 * @property {Array<any>} [items]
+	 * @property {any} [value]
+	 * @property {import('@rokkit/core').FieldMapping} [fields]
+	 * @property {Object} [icons]
+	 * @property {boolean} [autoCloseSiblings=false]
+	 * @property {boolean} [multiselect=false]
+	 * @property {Function} [header]
+	 * @property {Function} [footer]
+	 * @property {Function} [empty]
+	 * @property {Function} [stub]
+	 */
 
-  /** @type {Props & { [key: string]: any }} */
-  let {
-    class: classes = 'h-full overflow-scroll flex flex-col',
-    items = $bindable([]),
-    value = $bindable(null),
-    fields,
-    icons = {},
-    autoCloseSiblings = false,
-    multiselect = false,
-    header,
-    footer,
-    empty,
-    stub,
-    ...events
-  } = $props()
+	/** @type {Props & { [key: string]: any }} */
+	let {
+		class: classes = 'h-full overflow-scroll flex flex-col',
+		items = $bindable([]),
+		value = $bindable(null),
+		fields,
+		icons = {},
+		autoCloseSiblings = false,
+		multiselect = false,
+		header,
+		footer,
+		empty,
+		stub,
+		...events
+	} = $props()
 
-  let emitter = createEmitter(events, ['select', 'move', 'toggle'])
-  let wrapper = new NestedController(items, value, fields, { autoCloseSiblings, multiselect })
-  let snippets = omit(['onselect', 'onmove', 'ontoggle'], events)
-  let derivedFields = $derived({ ...defaultFields, ...fields })
+	let emitter = createEmitter(events, ['select', 'move', 'toggle'])
+	let wrapper = new NestedController(items, value, fields, { autoCloseSiblings, multiselect })
+	let snippets = omit(['onselect', 'onmove', 'ontoggle'], events)
+	let derivedFields = $derived({ ...defaultFields, ...fields })
 
-  function handleAction(event) {
-    const { name, data } = event.detail
-    if (name === 'select') value = data.value
-    if (has([name], emitter)) emitter[name](data)
-  }
+	function handleAction(event) {
+		const { name, data } = event.detail
+		if (name === 'select') value = data.value
+		if (has([name], emitter)) emitter[name](data)
+	}
 </script>
 
+<!-- svelte-ignore a11y_no_noninteractive_tabindex -->
 <div
-  data-tree-root
-  tabindex="0"
-  class={classes}
-  use:navigator={{ wrapper, nested: true }}
-  onaction={handleAction}
+	data-tree-root
+	tabindex="0"
+	class={classes}
+	use:navigator={{ wrapper, nested: true }}
+	onaction={handleAction}
 >
-  {#if header}
-    <div data-tree-header>{@render header()}</div>
-  {/if}
-  {#if items.length === 0}
-    {#if empty}
-      {@render empty()}
-    {:else}
-      <div class="m-auto p-4 text-center text-gray-500" data-tree-empty>No data available</div>
-    {/if}
-  {/if}
-  <List
-    items={items}
-    fields={derivedFields}
-    value={value}
-    icons={icons}
-    focusedKey={wrapper.currentKey}
-    selectedKeys={wrapper.selectedKeys}
-    stub={stub}
-    snippets={snippets}
-  />
-  {#if footer}
-    <div data-tree-footer>{@render footer()}</div>
-  {/if}
+	{#if header}
+		<div data-tree-header>{@render header()}</div>
+	{/if}
+	{#if items.length === 0}
+		{#if empty}
+			{@render empty()}
+		{:else}
+			<div class="m-auto p-4 text-center text-gray-500" data-tree-empty>No data available</div>
+		{/if}
+	{/if}
+	<List
+		{items}
+		fields={derivedFields}
+		{value}
+		{icons}
+		focusedKey={wrapper.currentKey}
+		selectedKeys={wrapper.selectedKeys}
+		{stub}
+		{snippets}
+	/>
+	{#if footer}
+		<div data-tree-footer>{@render footer()}</div>
+	{/if}
 </div>
