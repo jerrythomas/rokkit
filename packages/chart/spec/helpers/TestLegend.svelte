1314db92796bdefe6d2f4a0bba51536042e12c7f
Jerry Thomas <me@jerrythomas.name>
2026-03-23 22:45:37 -0500

    feat(chart): add plot/Legend.svelte with categorical/gradient modes

---
+++ packages/chart/spec/helpers/TestLegend.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { setContext } from 'svelte'
+  import Legend from '../../src/Plot/Legend.svelte'
+  let { state } = $props()
+  setContext('plot-state', state)
+</script>
+<Legend />
